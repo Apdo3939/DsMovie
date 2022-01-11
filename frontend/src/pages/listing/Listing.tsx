e4e2786a141e2a1762bdd31a793535246559560c
Apdo3939 <apdo3939@hotmail.com>
2022-01-11 16:36:39 -0300

    Add Pagination

--- frontend/src/pages/listing/Listing.tsx
+++ frontend/src/pages/listing/Listing.tsx
@@ -1,6 +1,13 @@
+import Pagination from "components/pagination/Pagination";
+import "./styles.css";
+
 function Listing() {
   return (
     <>
+      <div className="dsmovie-listing-container">
+        <Pagination />
+      </div>
+
       <h1>Listing</h1>
     </>
   );
