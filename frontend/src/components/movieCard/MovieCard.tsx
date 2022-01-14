5d9f3ef47c7ec6aed803f87dc917aebd5ea1b715
Apdo3939 <apdo3939@hotmail.com>
2022-01-14 19:21:49 -0300

    Add First request

--- frontend/src/components/movieCard/MovieCard.tsx
+++ frontend/src/components/movieCard/MovieCard.tsx
@@ -1,16 +1,16 @@
 import MovieScore from "components/movieScore/MovieScore";
 import { Link } from "react-router-dom";
 
-const movie = {
-  id: 1,
-  image:
-    "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-  title: "The Witcher",
-  count: 2,
-  score: 4.5,
-};
-
 function MovieCard() {
+  const movie = {
+    id: 1,
+    image:
+      "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+    title: "The Witcher",
+    count: 2,
+    score: 4.5,
+  };
+
   return (
     <div>
       <img
--- frontend/src/pages/listing/Listing.tsx
+++ frontend/src/pages/listing/Listing.tsx
@@ -1,8 +1,17 @@
+import axios from "axios";
 import MovieCard from "components/movieCard/MovieCard";
 import Pagination from "components/pagination/Pagination";
+import { BASE_URL } from "utils/Utils";
 import "./styles.css";
 
 function Listing() {
+  axios
+    .get(`${BASE_URL}/movies?size=5`)
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch(() => {});
+
   return (
     <>
       <div className="dsmovie-listing-container">
